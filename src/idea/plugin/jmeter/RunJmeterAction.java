6fed385ef268f4687a848a53ff52322136def553
Andrey Ponomarev <ponomandr@gmail.com>
2012-05-20 12:11:32 +0200

    Renamed OpenWithJmeterAction to RunJmeterAction. Changed icon.

--- src/idea/plugin/jmeter/RunJmeterAction.java
+++ src/idea/plugin/jmeter/RunJmeterAction.java
@@ -12,6 +12,8 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import idea.plugin.jmeter.run.JmeterConfigurationType;
 import idea.plugin.jmeter.run.JmeterRunConfiguration;
@@ -19,10 +21,10 @@ import idea.plugin.jmeter.run.JmeterRunConfiguration;
 import static idea.plugin.jmeter.settings.JmeterSettings.getJmeterJar;
 
 
-public class OpenWithJmeterAction extends AnAction {
+public class RunJmeterAction extends AnAction {
 
-    public OpenWithJmeterAction() {
-        super("Open with JMeter", null, JmeterFileType.FILE_ICON);
+    public RunJmeterAction() {
+        super("R&un", null, IconLoader.getIcon("/general/toolWindowRun.png"));
     }
 
     @Override
@@ -32,6 +34,7 @@ public class OpenWithJmeterAction extends AnAction {
         Project project = event.getData(PlatformDataKeys.PROJECT);
         event.getPresentation().setVisible(isJmeterFile(file));
         event.getPresentation().setEnabled(getJmeterJar(project).exists());
+        event.getPresentation().setText(getText(file), true);
     }
 
     @Override
@@ -45,6 +48,14 @@ public class OpenWithJmeterAction extends AnAction {
 
     private boolean isJmeterFile(VirtualFile file) {
         return file != null && JmeterFileType.INSTANCE.equals(file.getFileType());
+    }
+
+    private String getText(VirtualFile file) {
+        if (file == null) {
+            return getTemplatePresentation().getTextWithMnemonic();
+        } else {
+            return getTemplatePresentation().getTextWithMnemonic() + " '" + StringUtil.first(file.getNameWithoutExtension(), 30, true) + "'";
+        }
     }
 
     private void openWithJMeter(VirtualFile file, Project project) {
